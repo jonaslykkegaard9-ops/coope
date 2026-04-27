50499395b1bf00458680780300d49ed4176100bd
jonaslykkegaard9-ops <jonaslykkegaard9@gmail.com>
2026-04-27 23:11:11 +0200

    Refactor preprocessor directives in stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -8,17 +8,15 @@
 #	pragma clang diagnostic ignored "-Wmicrosoft-anon-tag" 
 #	pragma clang diagnostic ignored "-Wunused-function"
 #	define _CRT_SECURE_NO_WARNINGS 1  
+
 #	define STRICT
 #		include <Windows.h> 
 #	undef STRICT
-
 #	include <stdio.h>
 #	include <stdlib.h> 
 #	include <wchar.h> 
 #	include <stdint.h> 
-
 	typedef enum{ false, true }bool; 
-
 #	undef interface 
 #	undef THIS 
 #endif
